a607b571d7ab1345adb11bf4a4aa9c2902483e32
Thanh <thanh.chi.la@gmail.com>
2023-07-23 23:55:10 -0700

    Updates

---
+++ precache-manifest.57048eb05285f7ba7eb873b80f012577.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f53a18f2fecb4675c936",
+    "url": "/thanhla92/project-recipe.git/static/css/main.5a4ffcd3.chunk.css"
+  },
+  {
+    "revision": "f53a18f2fecb4675c936",
+    "url": "/thanhla92/project-recipe.git/static/js/main.f53a18f2.chunk.js"
+  },
+  {
+    "revision": "a804b799ace910feea6d",
+    "url": "/thanhla92/project-recipe.git/static/js/1.a804b799.chunk.js"
+  },
+  {
+    "revision": "2f5c4f7eb85adf90542a",
+    "url": "/thanhla92/project-recipe.git/static/js/runtime~main.2f5c4f7e.js"
+  },
+  {
+    "revision": "e9cacd4e7e8c6aa055975973f54cbacc",
+    "url": "/thanhla92/project-recipe.git/index.html"
+  }
+];
