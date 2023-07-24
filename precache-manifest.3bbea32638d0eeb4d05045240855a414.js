9893d3b5bcd9e032c61f6832a7cf3daea41e2678
Thanh <thanh.chi.la@gmail.com>
2023-07-24 00:00:11 -0700

    Fixing homepage for package.json

---
+++ precache-manifest.3bbea32638d0eeb4d05045240855a414.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a673a73b66b455af0b7",
+    "url": "/project-recipe/static/css/main.c6ed653b.chunk.css"
+  },
+  {
+    "revision": "4a673a73b66b455af0b7",
+    "url": "/project-recipe/static/js/main.42f0c4fe.chunk.js"
+  },
+  {
+    "revision": "ed7b99b8e50660f041e0",
+    "url": "/project-recipe/static/js/runtime~main.e2d25596.js"
+  },
+  {
+    "revision": "925064fe5cac0b4f4012",
+    "url": "/project-recipe/static/js/2.62b63465.chunk.js"
+  },
+  {
+    "revision": "fa2150ac7f798e15cef9d9bd105038fa",
+    "url": "/project-recipe/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/thanhla92/project-recipe.git/precache-manifest.57048eb05285f7ba7eb873b80f012577.js"
+  "/project-recipe/precache-manifest.3bbea32638d0eeb4d05045240855a414.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/thanhla92/project-recipe.git/index.html", {
+workbox.routing.registerNavigationRoute("/project-recipe/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
